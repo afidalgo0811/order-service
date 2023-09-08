f4efba579ceb32447edee3e795e3a26bbe535c26
afidalgoSE <afidalgose@gmail.com>
2023-09-08 02:48:14 -0400

    Initial commit

---
+++ src/main/kotlin/com/afidalgo/orderservice/OrderServiceApplication.kt
@@ -0,0 +1,10 @@
+package com.afidalgo.orderservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class OrderServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<OrderServiceApplication>(*args)
+}
