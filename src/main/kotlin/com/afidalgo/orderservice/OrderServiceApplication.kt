e6cad24c537c54bd596b2989243cf57ed9ed7d71
afidalgoSE <afidalgose@gmail.com>
2023-09-17 04:38:01 -0400

    wip

--- src/main/kotlin/com/afidalgo/orderservice/OrderServiceApplication.kt
+++ src/main/kotlin/com/afidalgo/orderservice/OrderServiceApplication.kt
@@ -3,8 +3,9 @@ package com.afidalgo.orderservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication class OrderServiceApplication
+@SpringBootApplication
+class OrderServiceApplication
 
 fun main(args: Array<String>) {
-  runApplication<OrderServiceApplication>(*args)
+    runApplication<OrderServiceApplication>(*args)
 }
