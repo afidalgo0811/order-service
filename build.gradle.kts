19a6a3787806ba28cea43e0a59b9c9250259a843
afidalgoSE <afidalgose@gmail.com>
2023-09-23 22:46:44 -0400

    wip 3 chapter 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.1.3"
-  id("io.spring.dependency-management") version "1.1.3"
-  kotlin("jvm") version "1.8.22"
-  kotlin("plugin.spring") version "1.8.22"
-  id("com.diffplug.spotless") version "6.19.0"
-  kotlin("kapt") version "1.8.0"
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    id("com.diffplug.spotless") version "6.19.0"
+    kotlin("kapt") version "1.8.0"
 }
 
 group = "com.afidalgo"
@@ -20,55 +20,76 @@ extra["testcontainersVersion"] = "1.18.0"
 
 java { sourceCompatibility = JavaVersion.VERSION_17 }
 
-repositories { mavenCentral() }
+java { targetCompatibility = JavaVersion.VERSION_17 }
+
+val spaceUsername: String? by project
+val spacePassword: String? by project
+val userName: String? = System.getenv("SPACE_USERNAME")
+val passWord: String? = System.getenv("SPACE_PASSWORD")
+val usr = userName ?: spaceUsername // checks env first
+val psw = passWord ?: spacePassword // checks env first
+val urlArtifactRepository = ext["jetbrains.url"].toString()
+val sharedLibraryVersion = ext["shared.library.version"].toString()
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri(urlArtifactRepository)
+        credentials {
+            username = usr
+            password = psw
+        }
+    }
+}
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-  implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-  runtimeOnly("org.postgresql:postgresql")
-  runtimeOnly("org.postgresql:r2dbc-postgresql")
-  runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.springframework:spring-jdbc")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("org.springframework.boot:spring-boot-testcontainers")
-  testImplementation("io.projectreactor:reactor-test")
-  testImplementation("org.testcontainers:junit-jupiter")
-  testImplementation("org.testcontainers:postgresql")
-  testImplementation("org.testcontainers:r2dbc")
-  kapt("org.springframework.boot:spring-boot-configuration-processor")
-  implementation("org.springframework.cloud:spring-cloud-starter-config")
-  implementation("org.springframework.retry:spring-retry")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
+    runtimeOnly("org.flywaydb:flyway-core")
+    runtimeOnly("org.springframework:spring-jdbc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:r2dbc")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.retry:spring-retry")
+    implementation("com.afidalgo:shared-library:$sharedLibraryVersion")
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    freeCompilerArgs += "-Xjsr305=strict"
-    jvmTarget = "17"
-  }
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 configure<SpotlessExtension> {
-  kotlin {
-    // by default the target is every '.kt' and '.kts` file in the java sourcesets
-    ktfmt() // has its own section below
-  }
-  kotlinGradle {
-    target("*.gradle.kts")
-    ktfmt()
-  }
+    kotlin {
+        // by default the target is every '.kt' and '.kts` file in the java sourcesets
+        ktfmt() // has its own section below
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktfmt()
+    }
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
 
 dependencyManagement {
-  imports {
-    mavenBom(
-        "org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
-  }
+    imports {
+        mavenBom(
+            "org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+    }
 }
