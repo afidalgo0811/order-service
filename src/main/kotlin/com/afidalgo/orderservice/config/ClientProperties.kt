7254d37f0e9e943859d45048615cbcbc60042d2c
afidalgoSE <afidalgose@gmail.com>
2023-09-26 03:26:03 -0400

    finished 8.3

--- src/main/kotlin/com/afidalgo/orderservice/config/ClientProperties.kt
+++ src/main/kotlin/com/afidalgo/orderservice/config/ClientProperties.kt
@@ -1,10 +1,12 @@
 package com.afidalgo.orderservice.config
 
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.net.URI
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "polar")
 data class ClientProperties(
     @field:NotNull val catalogServiceUri: URI,
+    @field:NotNull @field:Positive val timeoutValue: Int,
 )
