e95086a54b1d1b58565e050bfeb555f77f16dde9
afidalgoSE <afidalgose@gmail.com>
2023-09-27 03:48:32 -0400

    added tests 2

--- src/test/kotlin/com/afidalgo/orderservice/BookClientTests.kt
+++ src/test/kotlin/com/afidalgo/orderservice/BookClientTests.kt
@@ -7,44 +7,45 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 import shared.library.order.Book
 
-@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.Random::class)
 class BookClientTests() {
 
-  private val mockWebServer: MockWebServer = MockWebServer()
-  private lateinit var bookClient: BookClient
-  private var clientProperties: ClientProperties = ClientProperties()
+    private val mockWebServer: MockWebServer = MockWebServer()
+    private lateinit var bookClient: BookClient
+    private var clientProperties: ClientProperties = ClientProperties()
 
-  @BeforeEach
-  @Throws(IOException::class)
-  fun setUp() {
-    mockWebServer.start()
-    val webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build()
-    bookClient = BookClient(webClient, clientProperties)
-  }
+    @BeforeEach
+    @Throws(IOException::class)
+    fun setUp() {
+        mockWebServer.start()
+        val webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build()
+        bookClient = BookClient(webClient, clientProperties)
+    }
 
-  @AfterEach
-  @Throws(IOException::class)
-  fun clean() {
-    mockWebServer.shutdown()
-  }
+    @AfterEach
+    @Throws(IOException::class)
+    fun clean() {
+        mockWebServer.shutdown()
+    }
 
-  @Test
-  fun whenBookExistsThenReturnBook() {
-    val bookIsbn = "1234567890"
-    val mockResponse =
-        MockResponse()
-            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .setBody(
-                """
+    @Test
+    fun whenBookExistsThenReturnBook() {
+        val bookIsbn = "1234567890"
+        val mockResponse =
+            MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(
+                    """
                 {   "isbn":%s,
                     "title":"Title",
                     "author":"Author",
@@ -52,9 +53,9 @@ class BookClientTests() {
                     "publisher": "PolarSophia"
                 }
                 """
-                    .format(bookIsbn))
-    mockWebServer.enqueue(mockResponse)
-    val book: Mono<Book> = bookClient.getBookByIsbn(bookIsbn)
-    StepVerifier.create(book).expectNextMatches { b -> b.isbn == bookIsbn }.verifyComplete()
-  }
+                        .format(bookIsbn))
+        mockWebServer.enqueue(mockResponse)
+        val book: Mono<Book> = bookClient.getBookByIsbn(bookIsbn)
+        StepVerifier.create(book).expectNextMatches { b -> b.isbn==bookIsbn }.verifyComplete()
+    }
 }
