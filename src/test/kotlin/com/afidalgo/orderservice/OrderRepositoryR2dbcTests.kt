7ba2031521e3328d1b7027192ee012136965f14f
afidalgoSE <afidalgose@gmail.com>
2023-09-28 01:14:14 -0400

    finished testing

--- src/test/kotlin/com/afidalgo/orderservice/OrderRepositoryR2dbcTests.kt
+++ src/test/kotlin/com/afidalgo/orderservice/OrderRepositoryR2dbcTests.kt
@@ -1,8 +1,66 @@
 package com.afidalgo.orderservice
 
 import com.afidalgo.orderservice.config.DataConfig
+import com.afidalgo.orderservice.order.domain.OrderRepository
+import com.afidalgo.orderservice.order.domain.OrderService
+import com.afidalgo.orderservice.order.domain.OrderStatus
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import reactor.test.StepVerifier
+import shared.library.order.Book
 
-@DataR2dbcTest @Import(DataConfig::class) @Testcontainers class OrderRepositoryR2dbcTests {}
+@DataR2dbcTest
+@Import(DataConfig::class)
+@Testcontainers
+class OrderRepositoryR2dbcTests(@Autowired val orderRepository: OrderRepository) {
+  companion object {
+    @Container val postgreSQLContainer = PostgreSQLContainer(DockerImageName.parse("postgres:14.4"))
+
+    @JvmStatic
+    private fun r2dbcUrl() =
+        String.format(
+            "r2dbc:postgresql://%s:%s/%s",
+            postgreSQLContainer.host,
+            postgreSQLContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+            postgreSQLContainer.databaseName)
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun postgresqlProperties(registry: DynamicPropertyRegistry) {
+      registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl)
+      registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername)
+      registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword)
+      registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl)
+    }
+  }
+
+  @Test
+  fun createRejectedOrder() {
+    val rejectedOrder = OrderService.buildRejectedOrder("1234567890", 3)
+    StepVerifier.create(orderRepository.save(rejectedOrder))
+        .expectNextMatches { order -> order.status == OrderStatus.REJECTED }
+        .verifyComplete()
+  }
+
+  @Test
+  fun createAcceptedOrder() {
+    val acceptedOrder =
+        OrderService.buildAcceptedOrder(Book("1234567890", "Title", "Author", 9.0), 3)
+    StepVerifier.create(orderRepository.save(acceptedOrder))
+        .expectNextMatches { order -> order.status == OrderStatus.ACCEPTED }
+        .verifyComplete()
+  }
+
+  @Test
+  fun findOrderByIdWhenNotExisting() {
+    StepVerifier.create(orderRepository.findById(394L)).expectNextCount(0).verifyComplete()
+  }
+}
---
+++ src/test/kotlin/com/afidalgo/orderservice/OrderServiceApplicationTests.kt
@@ -0,0 +1,128 @@
+package com.afidalgo.orderservice
+
+import com.afidalgo.orderservice.book.BookClient
+import com.afidalgo.orderservice.order.domain.Order
+import com.afidalgo.orderservice.order.domain.OrderStatus
+import com.afidalgo.orderservice.order.web.OrderRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import reactor.core.publisher.Mono
+import shared.library.order.Book
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class OrderServiceApplicationTests(
+    @Autowired val webTestClient: WebTestClient,
+) {
+  @MockBean lateinit var bookClient: BookClient
+
+  companion object {
+    @Container val postgreSQLContainer = PostgreSQLContainer(DockerImageName.parse("postgres:14.4"))
+
+    @JvmStatic
+    private fun r2dbcUrl() =
+        String.format(
+            "r2dbc:postgresql://%s:%s/%s",
+            postgreSQLContainer.host,
+            postgreSQLContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+            postgreSQLContainer.databaseName)
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun postgresqlProperties(registry: DynamicPropertyRegistry) {
+      registry.add("spring.r2dbc.url", OrderServiceApplicationTests::r2dbcUrl)
+      registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername)
+      registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword)
+      registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl)
+    }
+  }
+
+  @Test
+  fun whenGetOrdersThenReturn() {
+    val bookIsbn = "1234567893"
+    val book = Book(bookIsbn, "Title", "Author", 9.90)
+    given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book))
+    val orderRequest = OrderRequest(bookIsbn, 1)
+    val expectedOrder: Order? =
+        webTestClient
+            .post()
+            .uri("/orders")
+            .bodyValue(orderRequest)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBody(Order::class.java)
+            .returnResult()
+            .responseBody
+    assertThat(expectedOrder).isNotNull()
+    webTestClient
+        .get()
+        .uri("/orders")
+        .exchange()
+        .expectStatus()
+        .is2xxSuccessful()
+        .expectBodyList(Order::class.java)
+        .value<WebTestClient.ListBodySpec<Order>> { orders ->
+          assertThat(orders.stream().filter { order -> order.bookIsbn == bookIsbn }.findAny())
+              .isNotEmpty
+        }
+  }
+
+  @Test
+  fun whenPostRequestAndBookExistsThenOrderAccepted() {
+    val bookIsbn = "1234567899"
+    val book = Book(bookIsbn, "Title", "Author", 9.90)
+    given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book))
+    val orderRequest = OrderRequest(bookIsbn, 3)
+    val createdOrder: Order? =
+        webTestClient
+            .post()
+            .uri("/orders")
+            .bodyValue(orderRequest)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBody(Order::class.java)
+            .returnResult()
+            .responseBody
+    assertThat(createdOrder).isNotNull()
+    assertThat(createdOrder?.bookIsbn).isEqualTo(orderRequest.isbn)
+    assertThat(createdOrder?.quantity).isEqualTo(orderRequest.quantity)
+    assertThat(createdOrder?.bookName).isEqualTo(book.title + "-" + book.author)
+    assertThat(createdOrder?.bookPrice).isEqualTo(book.price)
+    assertThat(createdOrder?.status).isEqualTo(OrderStatus.ACCEPTED)
+  }
+
+  @Test
+  fun whenPostRequestAndBookNotExistsThenOrderRejected() {
+    val bookIsbn = "1234567894"
+    given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.empty())
+    val orderRequest = OrderRequest(bookIsbn, 3)
+    val createdOrder: Order? =
+        webTestClient
+            .post()
+            .uri("/orders")
+            .bodyValue(orderRequest)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBody(Order::class.java)
+            .returnResult()
+            .responseBody
+    assertThat(createdOrder).isNotNull()
+    assertThat(createdOrder?.bookIsbn).isEqualTo(orderRequest.isbn)
+    assertThat(createdOrder?.quantity).isEqualTo(orderRequest.quantity)
+    assertThat(createdOrder?.status).isEqualTo(OrderStatus.REJECTED)
+  }
+}
